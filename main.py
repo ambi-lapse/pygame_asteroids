2d44e604a94ffb5838afdd31cdf139210c6b71bb
ambi-lapse <kylhiggins@gmail.com>
2025-08-11 23:32:45 -0700

    player drawn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame
-
+import pygame 
 from constants import *
+from player import *
+
 
 
 
@@ -12,12 +13,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        rt = game_clock.tick(60)
+        dt = rt/1000
 
 
 if __name__ == "__main__":
